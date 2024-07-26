db9679f1f0657732331c67d6d33392e473935cef
zerocoder524 <vandro987@gmail.com>
2024-07-26 14:23:35 +0300

    rename images directory

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Игра Тир')
-icon = pygame.image.load('image/images.jpg')
+icon = pygame.image.load('img/images.jpg')
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load('image/target.png')
+target_img = pygame.image.load('img/target.png')
 target_width = 80
 target_height = 80
 
